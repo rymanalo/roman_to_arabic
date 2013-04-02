90cf189ed78a22e4eb90f249145b06a0a8a32359
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-04-02 01:21:41 -0700

    Fix indentation.

--- roman_to_arabic.rb
+++ roman_to_arabic.rb
@@ -1,16 +1,16 @@
 require "test/unit"
 class ArabicGenerator 
-   TABLE = {
-      "I"  => 1,
-      "V"  => 5,
-      "X"  => 10,
-      "L"  => 50,
-      "C"  => 100,
-      "D"  => 500,
-      "M"  => 1000
-    }
+  TABLE = {
+    "I"  => 1,
+    "V"  => 5,
+    "X"  => 10,
+    "L"  => 50,
+    "C"  => 100,
+    "D"  => 500,
+    "M"  => 1000
+  }
 
-   def convert(roman_number)
+  def convert(roman_number)
     arabic_num = 0
     char = roman_number.split("")
 
@@ -24,41 +24,41 @@ class ArabicGenerator
      	else
      		arabic_num += TABLE[char.shift]
      	end
-     end
-     return arabic_num 
-   end
-   
+    end
+    arabic_num 
+  end
 end
+
 class ArabicGeneratorTest < Test::Unit::TestCase
-   def setup
-      @converter = ArabicGenerator.new
-   end
-   def test_return_1
-      assert_equal 1, @converter.convert("I")
-   end
-   def test_return_2
-      assert_equal 2, @converter.convert("II")
-   end
-   def test_return_3
-      assert_equal 3, @converter.convert("III")
-   end
-   def test_return_4
-      assert_equal 4, @converter.convert("IV")
-   end
+  def setup
+    @converter = ArabicGenerator.new
+  end
+  def test_return_1
+    assert_equal 1, @converter.convert("I")
+  end
+  def test_return_2
+    assert_equal 2, @converter.convert("II")
+  end
+  def test_return_3
+    assert_equal 3, @converter.convert("III")
+  end
+  def test_return_4
+    assert_equal 4, @converter.convert("IV")
+  end
 
-   def test_return_9
-      assert_equal 9, @converter.convert("IX")
-   end
+  def test_return_9
+    assert_equal 9, @converter.convert("IX")
+  end
 
-   def test_return_57
-      assert_equal 57, @converter.convert("LVII")
-   end
+  def test_return_57
+    assert_equal 57, @converter.convert("LVII")
+  end
 
-   def test_return_49
-      assert_equal 49, @converter.convert("XLIX")
-   end
+  def test_return_49
+    assert_equal 49, @converter.convert("XLIX")
+  end
 
-   def test_return_crazy_num
-      assert_equal 1949, @converter.convert("MCMXLIX")
-   end
+  def test_return_crazy_num
+    assert_equal 1949, @converter.convert("MCMXLIX")
+  end
 end
